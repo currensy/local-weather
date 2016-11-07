f07aeb814e8b7e7fd98b7d845aca55b7bc28a19a
unknown <iabramov@L-TLV-00448529.corp.ebay.com>
2016-11-07 15:40:50 +0200

    finalized

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 
   var weatherApi = "http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID=9ba037edaaa09f9ce875786979b66dc6&units=metric";
   var weatherIconUrl = "http://openweathermap.org/img/w/{id}.png";
+  var temp = {};
+  var roundedTemparature = 0;
+  var tempInFahrenheit = 0;
 
   var weatherMapTable = {
     2: 'wi-thunderstorm',
@@ -24,31 +27,56 @@ $(document).ready(function() {
         var weather = data.weather[0].main;
         var icon = data.weather[0].icon;
         var weatherId = (data.weather[0].id == 800) ? 800 : data.weather[0].id[0];
-        var temp = {
+        roundedTemparature = Math.round(data.main.temp);
+        tempInFahrenheit = celsiusToFahrenheit(roundedTemparature);
+        temp = {
           degrees: Math.round(data.main.temp),
           unit: "celsius"
         };
 
         $("#location").html(cityName + ", " + country);
         $("#temp").html(temp.degrees + " " + setWeatherMetrics(temp));
-        $("#weather-text").html(weather);
-        $("#icon").html('<i class="wi ' + weatherMapTable[weatherId] + '"></i>');
+        $("#weather-text").html(weather).append('<i class="wi ' + weatherMapTable[weatherId] + '"></i>');
+        //$("#icon").html('<i class="wi ' + weatherMapTable[weatherId] + '"></i>');
         //$("#icon").css('background-image', 'url(' + weatherIconUrl.replace('{id}', icon) + ')');
 
       });
+
       $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
 
 
     });
   }
 
+  $("#temp").click(function(){
+    changeMetric(temp);
+    $("#temp").html(temp.degrees + " " + setWeatherMetrics(temp));
+  });
+
+
+  function changeMetric(temp){
+    if (isCelsius(temp.unit)) {
+      temp.degrees = tempInFahrenheit;
+      temp.unit = "fahrenheit";
+    } else {
+      temp.degrees = roundedTemparature;
+      temp.unit = "celsius";
+    }
+  }
+
+  function isCelsius(temp){
+    return temp==="celsius";
+  }
+
+  function celsiusToFahrenheit(temp){
+    return Math.round((temp*9)/5+32);
+  }
+
+  function setWeatherMetrics(temp) {
+    var i = '<i class="wi {metric-icon}"></i>';
+    return (temp.unit === "celsius") ? i.replace("{metric-icon}", "wi-celsius") : i.replace("{metric-icon}", "wi-fahrenheit");
+
+  }
 
 
 });
-
-
-function setWeatherMetrics(temp) {
-  var i = '<i class="wi {metric-icon}"></i>';
-  return (temp.unit === "celsius") ? i.replace("{metric-icon}", "wi-celsius") : i.replace("{metric-icon}", "wi-fahrenheit");
-
-}
