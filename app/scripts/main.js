1af3260cca17ee3475994862ed3becc951b203f5
iabramov <iabramov@ebay.com>
2016-11-08 17:25:58 +0200

    refactor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,76 +7,59 @@ $(document).ready(function() {
   var roundedTemparature = 0;
   var tempInFahrenheit = 0;
 
-  var weatherMapTable = {
-    2: 'wi-thunderstorm',
-    3: 'wi-raindrops',
-    5: 'wi-rain',
-    6: 'wi-snow',
-    800: 'wi-day-sunny',
-    8: 'wi-cloudy'
-  };
 
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var url = weatherApi.replace('{lat}', position.coords.latitude).replace('{lon}', position.coords.longitude);
-      console.log(weatherApi.replace('{lat}', position.coords.latitude).replace('{lon}', position.coords.longitude));
-      $.get(url, function(data) {
-        var cityName = data.name;
-        var country = data.sys.country;
-        var weather = data.weather[0].main;
-        var icon = data.weather[0].icon;
-        var weatherId = (data.weather[0].id == 800) ? 800 : data.weather[0].id[0];
-        roundedTemparature = Math.round(data.main.temp);
-        tempInFahrenheit = celsiusToFahrenheit(roundedTemparature);
-        temp = {
-          degrees: Math.round(data.main.temp),
-          unit: "celsius"
-        };
-
-        $("#location").html(cityName + ", " + country);
-        $("#temp").html(temp.degrees + " " + setWeatherMetrics(temp));
-        $("#weather-text").html(weather).append('<i class="wi ' + weatherMapTable[weatherId] + '"></i>');
-        //$("#icon").html('<i class="wi ' + weatherMapTable[weatherId] + '"></i>');
-        //$("#icon").css('background-image', 'url(' + weatherIconUrl.replace('{id}', icon) + ')');
-
-      });
-
-      $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-
-
+  getLocation().done(function(data) {
+    var loc = data.loc.split(",");
+    var lat = loc[0];
+    var lon = loc[1];
+    getWeather(lat, lon).done(function(weatherData) {
+      var weather = new Weather(weatherData.name, weatherData.sys.country, weatherData.weather[0].main, weatherData.weather[0].id);
+      console.log(weather.city);
     });
-  }
 
-  $("#temp").click(function(){
-    changeMetric(temp);
-    $("#temp").html(temp.degrees + " " + setWeatherMetrics(temp));
+
+
   });
 
 
-  function changeMetric(temp){
-    if (isCelsius(temp.unit)) {
-      temp.degrees = tempInFahrenheit;
-      temp.unit = "fahrenheit";
-    } else {
-      temp.degrees = roundedTemparature;
-      temp.unit = "celsius";
+  function Weather(city, country, weather, weatherId) {
+
+    this.city = city;
+    this.country = country;
+    this.weather = weather;
+    this.weatherId = weatherId;
+
+    this.getCity = function() {
+      return this.city;
     }
+
+    // getCity: function() {
+    //     return this.city;
+    //   },
+    //   getCountry: function() {
+    //     return this.country;
+    //   },
+    //   getWeather: function() {
+    //     return this.weather;
+    //   },
+    //   getweatherId: function() {
+    //     return this.weatherId;
+    //   }
+
+  };
+
+
+  function getWeather(lat, lon) {
+    var url = weatherApi.replace('{lat}', lat).replace('{lon}', lon);
+    console.log(url);
+    return $.get(url);
   }
 
-  function isCelsius(temp){
-    return temp==="celsius";
+  function getLocation() {
+    return $.getJSON('http://ipinfo.io');
   }
 
-  function celsiusToFahrenheit(temp){
-    return Math.round((temp*9)/5+32);
-  }
 
-  function setWeatherMetrics(temp) {
-    var i = '<i class="wi {metric-icon}"></i>';
-    return (temp.unit === "celsius") ? i.replace("{metric-icon}", "wi-celsius") : i.replace("{metric-icon}", "wi-fahrenheit");
-
-  }
 
 
 });
